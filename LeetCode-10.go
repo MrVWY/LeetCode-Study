826d96acef829b3bd1b3aa9082d64a4ab2a5b472
MrVWY <365273231@qq.com>
2019-12-21 20:26:41 +0800

    Update LeetCode-10.go

--- LeetCode-10.go
+++ LeetCode-10.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 //两数相加
 //输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
@@ -41,6 +44,38 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	}
 	return NewNode.Next
 }
+
+
+//给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+//输入: "abcabcbb"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+func lengthOfLongestSubstring(s string) int {
+	ans,start,end := 0,0,0
+	ma := make(map[byte]byte)
+	leng := len(s)
+	for start < leng && end < leng {
+		Value := s[end]
+		if _, ok := ma[Value] ; ok {
+			delete(ma , s[start])
+			start++
+		}else {
+			ma[s[end]] = s[end]
+			end++
+			ans = int(math.Max(float64(ans),float64(end-start)))
+			fmt.Println(ans,end,start)
+			//            1   1   0
+			//            2   2   0
+			//            3   3   0
+			//            3   4   1
+			//            3   5   2
+			//            3   6   3
+			//            3   7   5
+		}
+	}
+	return ans
+}
+
 
 func main() {
 	b := &ListNode{Val: 9, Next:nil}
