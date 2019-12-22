ca4000ee4dc331e7a2d736f5f85fd274325fcc5f
MrVWY <365273231@qq.com>
2019-12-22 18:15:52 +0800

    Create LeetCode-11.go

---
+++ LeetCode-11.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+//最长回文
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	last := len(s) - 1
+	longer := string(s[0])
+	for i := 0 ; i < last ; i++{
+		for j := 0 ; j <= last ; j++ {
+			if j - i + 1 > len(longer) && isPalindrome(s[i:j+1]) {
+				longer = s[i:j+1]
+			}
+		}
+	}
+	return longer
+}
+
+func isPalindrome(s string) bool {
+	mid := len(s) / 2
+	last := len(s) - 1
+	fmt.Println(len(s),mid,last)
+	for i := 0 ; i < mid ; i++ {
+		if s[i] != s[last-i] {
+			return false
+		}
+	}
+	return true
+}
+
+
+//回文串是左右对称的,依次以母串的每一个字符为中心轴,得到回文串;然后通过比较得到最长的那一个.   注意:要考虑到像baccab这样的特殊子串,可以理解它的中心轴是cc
+func longestPalindrome_One(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	length := len(s)
+	longer := string(s[0])
+	for i := 0 ; i < length - 1 ; i++ {
+		if len(isPalindrome_One(s,i)) > len(longer) {
+			longer = isPalindrome_One(s,i)
+		}
+	}
+	return longer
+}
+
+func isPalindrome_One(s string, mid int) string {
+	L := mid - 1
+	R := mid + 1
+	length := len(s)
+	for R < length && s[R] == s[mid] {
+		R ++
+	}
+	for L >= 0 && R < length && s[R] == s[L]{
+		L--
+		R++
+	}
+	return s[L+1:R]
+}
