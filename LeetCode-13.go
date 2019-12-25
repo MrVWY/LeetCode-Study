40fbdb26c25007e8f926a4a65cbe91deb7f448f4
MrVWY <365273231@qq.com>
2019-12-25 22:33:30 +0800

    Update LeetCode-13.go

--- LeetCode-13.go
+++ LeetCode-13.go
@@ -31,3 +31,44 @@ func Maxs(x,y int) int {
 	}
 	return y
 }
+
+//整数转罗马数字
+//字符          数值
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+//执行用时 :44 ms
+//内存消耗 :4.4 MB
+func intToRoman(num int) string {
+	nums := []int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
+	romans := []string{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"}
+	index := 0
+	result := ""
+	if num < 1 || num > 3999 {
+		return result
+	}
+	for {
+		if index >= 13 {
+			index--
+		}
+		if num == 0 {
+			break
+		}
+		if num >= nums[index] {
+			result += romans[index]
+			num -= nums[index]
+		}
+		if num < nums[index] {
+			index++
+		}
+		fmt.Println(index , num)
+	}
+	return result
+}
+
+
+
