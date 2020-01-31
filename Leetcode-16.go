639903d87a54bd0d97d8c0964d6ee8ca0899ddee
MrVWY <365273231@qq.com>
2020-01-31 17:26:20 +0800

    Update Leetcode-16.go

--- Leetcode-16.go
+++ Leetcode-16.go
@@ -54,3 +54,89 @@ func isAnagram(s string, t string) bool {
 	}
 	return true
 }
+
+//KMP  实现 strStr()例子   strings.Index()
+func strStr(haystack string, needle string) int {
+	haystackRune := []rune(haystack)
+	needleRune := []rune(needle)
+
+	M := len(haystackRune)
+	N := len(needleRune)
+	if N == 0 {
+		return 0
+	}
+	if M == 0 || M < N {
+		return -1
+	}
+	dp := kmp(needleRune)
+	j := 0
+	for i := 0; i < M; i++ {
+		j = dp[j][haystackRune[i]]
+		if j == N {
+			return i - N + 1
+		}
+	}
+	return -1
+}
+
+func kmp(needleRune []rune) []map[rune]int {
+
+	l := len(needleRune)
+
+	charMap := make(map[rune]int)
+	var firstChar rune
+	for i, s := range needleRune {
+		if i == 0 {
+			firstChar = s
+		}
+		charMap[s] = i
+	}
+	dp := make([]map[rune]int, l)
+	dp[0] = make(map[rune]int)
+	dp[0][firstChar] = 1
+	//影子状态 X 初始为 0
+	X := 0
+	for i := 1; i < l; i++ {
+		dp[i] = make(map[rune]int)
+		for sj := range charMap {
+			dp[i][sj] = dp[X][sj]
+		}
+		dp[i][needleRune[i]] = i + 1
+		// 更新影子状态
+		X = dp[X][needleRune[i]]
+	}
+
+	return dp
+}
+
+//最长公共前缀  (注意break用法)
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}else if len(strs) == 1 {
+		return strs[0]
+	}else if len(strs[0]) == 0 {
+		return ""
+	}
+	//判断最长前缀长度
+	maxNumber := len(strs[0])
+	for x:=1 ; x < len(strs) ; x++{
+		strNumber := len(strs[x])
+		if strNumber < maxNumber {
+			if strNumber == 0 {
+				return ""
+			}
+			maxNumber = strNumber
+		}
+	}
+	i:=0
+	J:
+	for ; i < maxNumber ; i++ {
+		for j := 1 ; j < len(strs) ; j++ {
+			if strs[j][i] != strs[j-1][i]{
+				break J
+			}
+		}
+	}
+	return strs[0][0:i]
+}
