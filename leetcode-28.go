30f26db31a4d1c758deac355d4ef9acd898fce09
MrVWY <365273231@qq.com>
2020-02-22 15:47:44 +0800

    Update leetcode-28.go

--- leetcode-28.go
+++ leetcode-28.go
@@ -98,3 +98,95 @@ func numOfSubarrays(arr []int, k int, threshold int) int {
 	}
 	return count
 }
+
+//直线上最多的点数
+type Point struct {
+	x, y int
+}
+
+type K struct {
+	k1 , k2 int
+}
+
+type B struct {
+	b1 , b2 int
+}
+
+type Line struct {
+	K
+	B
+	c int
+}
+
+//求最小公约数(辗转相除法)
+func gcd(a,b int) int {
+	for b != 0 {
+		a, b = b , a%b
+	}
+	return a
+}
+
+func NewLine(p,q Point) Line {
+	if p.x == q.x {
+		return Line{c: p.x}
+	}
+
+	k1 , k2 := p.y - q.y , p.x - q.x
+	g1 := gcd(k1,k2)
+	k := K{k1/g1,k2/g1}
+
+	b1 , b2 := q.y * k.k2 - k.k1 * q.x , k.k2   // b1 / b2  =  (y * k2 - k1 * x1) / k2
+	g2 := gcd(b1,b2)
+	b := B{b1/g2,b2/g2}
+
+	return Line{K: k, B: b}
+}
+
+func (L Line) HasPointInLine(p Point) bool {
+	if L.k2 == 0 {
+		//垂直X的直线
+		return p.x == L.c
+	}
+	return p.y * L.k2 * L.b2 == L.k1 * L.b2 * p.x + L.k2 * L.b1  //y * k2 * b2 = k1 * b1 * x + k2 * b1
+}
+
+func maxPoints(points [][]int) int {
+	length := len(points)
+	if length < 3 {return length}
+
+	i := 0
+	for ; i < length - 1 ; i++ {
+		if points[i][0] != points[i+1][0] || points[i][1] != points[i+1][1] {break}
+	}
+	if i == length - 1 { return length}
+
+	lines := make(map[Line]int)
+	var line Line
+	count := 0
+	for i := 0 ; i < length ; i++ {
+		for j := i + 1 ; j < length ; j++ {
+			if points[i][0] == points[j][0] && points[i][1] == points[j][1] {continue} // 避免相同的点出现
+
+			line = NewLine(Point{x: points[i][0], y: points[i][1]}, Point{x: points[j][0], y: points[j][1],})
+			//查看line 是否在之前已经出现过
+			if lines[line] != 0 {continue}
+
+			count = 2
+			//计算点是否在直线上
+			for index := 0 ; index < length ; index++ {
+				if index ==i || index ==j {continue}
+				if line.HasPointInLine(Point{points[index][0], points[index][1]}) {count++}
+			}
+
+			//记录
+			lines[line] = count
+		}
+	}
+	max := 0
+	for _ , v := range lines {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
