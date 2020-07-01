aa0548e617c3ab45a5f708e467440a2ef3a78476
MrVWY <365273231@qq.com>
2020-07-01 23:51:03 +0800

    Update tree1.go

--- Tree/tree1.go
+++ Tree/tree1.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 	"math"
 )
+//235. 二叉搜索树的最近公共祖先
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+
+	if p.Val > root.Val && q.Val > root.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	}else if p.Val < root.Val && q.Val < root.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	}else {
+		return root
+	}
+}
 
 //236. 二叉树的最近公共祖先
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
