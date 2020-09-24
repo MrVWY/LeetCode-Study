752046af0b5ccc5db02c942a91d2680a0d92327d
MrVWY <365273231@qq.com>
2020-09-25 00:33:28 +0800

    Create Preorder_Traverse.go

---
+++ Morris_Traverse/Preorder_Traverse.go
@@ -0,0 +1,28 @@
+package Morris_Traverse
+
+import "fmt"
+
+//前序遍历  跟-左-右
+
+func Preorder_Traverse(root *TreeNode) {
+	cur := root
+	for cur != nil {
+		if cur.Left != nil {
+			pre := cur.Left
+			for pre.Right != nil && pre.Right != cur {
+				pre = pre.Right
+			}
+			if pre.Right == nil {
+				pre.Right = cur
+				fmt.Println(cur.Val)
+				cur = cur.Left
+				continue
+			}else {
+				pre.Right = nil
+			}
+		}else {
+			fmt.Println(cur.Val)
+		}
+		cur = cur.Right
+	}
+}
